6d345c6b1c9a41856b33f10925bba7abfdb3fe1f
pkelbern <pkelbern@gmail.com>
2016-10-03 09:15:26 -0300

    CI
    Changes to pass the CI

--- src/services/calendarEventTitle.js
+++ src/services/calendarEventTitle.js
@@ -7,15 +7,21 @@ angular
   .factory('calendarEventTitle', function(calendarDateFilter, calendarTruncateEventTitleFilter) {
 
     function yearView(event) {
-	        return event.yearViewRender ? event.yearViewRenderRender(event) : event.title + ' (' + calendarDateFilter(event.startsAt, 'datetime', true) + ')';
+	        return event.yearViewRender ? 
+            event.yearViewRenderRender(event) : 
+            event.title + ' (' + calendarDateFilter(event.startsAt, 'datetime', true) + ')';
 	    }
 
 	    function monthView(event) {
-	        return event.monthViewRender ? event.monthViewRender(event) : event.title + ' (' + calendarDateFilter(event.startsAt, 'time', true) + ')';
+	        return event.monthViewRender ? 
+            event.monthViewRender(event) : 
+            event.title + ' (' + calendarDateFilter(event.startsAt, 'time', true) + ')';
 	    }
 
 	    function monthViewTooltip(event) {
-	        return event.monthViewTooltipRender ? event.monthViewTooltipRender(event) : (calendarDateFilter(event.startsAt, 'time', true) + ' - ' + event.title);
+	        return event.monthViewTooltipRender ? 
+            event.monthViewTooltipRender(event) : 
+            (calendarDateFilter(event.startsAt, 'time', true) + ' - ' + event.title);
 	    }
 
 	    function weekView(event) {
