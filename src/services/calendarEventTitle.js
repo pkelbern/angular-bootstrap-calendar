14b24241331179cc2cb6c08b1fcf48890d2a1b3c
pkelbern <pkelbern@gmail.com>
2016-10-03 09:24:53 -0300

    CI Travis
    CI- Trailing spaces not allowed  no-trailing-spaces

--- src/services/calendarEventTitle.js
+++ src/services/calendarEventTitle.js
@@ -7,20 +7,20 @@ angular
   .factory('calendarEventTitle', function(calendarDateFilter, calendarTruncateEventTitleFilter) {
 
     function yearView(event) {
-          return event.yearViewRender 
-            ? event.yearViewRenderRender(event)             
-            : event.title + ' (' + calendarDateFilter(event.startsAt, 'datetime', true) + ')';
+          return event.yearViewRender
+		  ? event.yearViewRenderRender(event)
+		  : event.title + ' (' + calendarDateFilter(event.startsAt, 'datetime', true) + ')';
       }
 
       function monthView(event) {
-          return event.monthViewRender             
-            ? event.monthViewRender(event)             
+          return event.monthViewRender
+            ? event.monthViewRender(event)
             : event.title + ' (' + calendarDateFilter(event.startsAt, 'time', true) + ')';
       }
 
       function monthViewTooltip(event) {
-          return event.monthViewTooltipRender 
-            ? event.monthViewTooltipRender(event) 
+          return event.monthViewTooltipRender
+            ? event.monthViewTooltipRender(event)
             : (calendarDateFilter(event.startsAt, 'time', true) + ' - ' + event.title);
       }
 
