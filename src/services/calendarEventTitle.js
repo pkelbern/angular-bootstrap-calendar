700efdcbf39c04f22d52677c207ec9595486eb40
pkelbern <pkelbern@gmail.com>
2016-10-03 09:20:45 -0300

    CI Travis
    Changes for CI Travis

--- src/services/calendarEventTitle.js
+++ src/services/calendarEventTitle.js
@@ -7,34 +7,34 @@ angular
   .factory('calendarEventTitle', function(calendarDateFilter, calendarTruncateEventTitleFilter) {
 
     function yearView(event) {
-	        return event.yearViewRender ? 
-            event.yearViewRenderRender(event) : 
-            event.title + ' (' + calendarDateFilter(event.startsAt, 'datetime', true) + ')';
-	    }
+          return event.yearViewRender 
+            ? event.yearViewRenderRender(event)             
+            : event.title + ' (' + calendarDateFilter(event.startsAt, 'datetime', true) + ')';
+      }
 
-	    function monthView(event) {
-	        return event.monthViewRender ? 
-            event.monthViewRender(event) : 
-            event.title + ' (' + calendarDateFilter(event.startsAt, 'time', true) + ')';
-	    }
+      function monthView(event) {
+          return event.monthViewRender             
+            ? event.monthViewRender(event)             
+            : event.title + ' (' + calendarDateFilter(event.startsAt, 'time', true) + ')';
+      }
 
-	    function monthViewTooltip(event) {
-	        return event.monthViewTooltipRender ? 
-            event.monthViewTooltipRender(event) : 
-            (calendarDateFilter(event.startsAt, 'time', true) + ' - ' + event.title);
-	    }
+      function monthViewTooltip(event) {
+          return event.monthViewTooltipRender 
+            ? event.monthViewTooltipRender(event) 
+            : (calendarDateFilter(event.startsAt, 'time', true) + ' - ' + event.title);
+      }
 
-	    function weekView(event) {
-	      return event.title;
-	    }
+      function weekView(event) {
+        return event.title;
+      }
 
-	    function weekViewTooltip(event) {
-	      return event.title;
-	    }
+      function weekViewTooltip(event) {
+        return event.title;
+      }
 
-	    function dayView(event) {
-	      return event.allDay ? event.title : calendarTruncateEventTitleFilter(event.title, 20, event.height);
-	    }
+      function dayView(event) {
+        return event.allDay ? event.title : calendarTruncateEventTitleFilter(event.title, 20, event.height);
+      }
 
     return {
       yearView: yearView,
